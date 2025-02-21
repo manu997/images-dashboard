1ee0f58642d535077905d37c6e26e2271c9dca1b
manu997 <manuelmorenoasiain@gmail.com>
2025-02-21 20:39:01 +0100

    Add images actions

--- src/components/ImageContainer/ImageContainer.tsx
+++ src/components/ImageContainer/ImageContainer.tsx
@@ -1,6 +1,11 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { Image } from "../../types";
 import "./imaged-container.css";
-
+import {
+  faHeart as faHeartOutline,
+  faPaperPlane,
+} from "@fortawesome/free-regular-svg-icons";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 interface ImageContainerProps {
   node: Image;
 }
@@ -8,13 +13,28 @@ interface ImageContainerProps {
 export const ImageContainer = ({ node }: ImageContainerProps) => {
   return (
     <figure key={node.title} className="image-container">
-      <div className="image-price-background">
-        <span className="image-price">
-          {node.price.toFixed(2)}
-          <small>€</small>
-        </span>
+      <div className="image-wrapper">
+        <div className="image-price-background">
+          <span className="image-price">
+            {node.price.toFixed(2)}
+            <small>€</small>
+          </span>
+        </div>
+        <img src={node.picture} alt={node.title} />
+        <div className="image-actions">
+          <div className="image-actions-item">
+            <FontAwesomeIcon
+              className="like-icon"
+              icon={node.liked ? faHeart : faHeartOutline}
+            />
+            <span>{node.likesCount}</span>
+          </div>
+          <div className="image-actions-item">
+            <FontAwesomeIcon icon={faPaperPlane} />
+            <span>0</span>
+          </div>
+        </div>
       </div>
-      <img src={node.picture} alt={node.title} />
       <figcaption>
         <h1 className="image-title">{node.title.toUpperCase()}</h1>
         <span className="author-label">by</span>
