d82de49dbb296e5f169335d5930736aac9c2aaa8
manu997 <manuelmorenoasiain@gmail.com>
2025-02-22 12:51:31 +0100

    Add like image feature

--- src/components/ImageContainer/ImageContainer.tsx
+++ src/components/ImageContainer/ImageContainer.tsx
@@ -1,12 +1,14 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import type { Image } from "../../types";
-import "./imaged-container.css";
 import {
   faHeart as faHeartOutline,
   faPaperPlane,
 } from "@fortawesome/free-regular-svg-icons";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useCallback, useState } from "react";
+import useLikeImage from "../../queries/useLikeImage";
+import type { Image } from "../../types";
+import "./imaged-container.css";
+
 interface ImageContainerProps {
   node: Image;
 }
@@ -14,9 +16,19 @@ interface ImageContainerProps {
 export const ImageContainer = ({ node }: ImageContainerProps) => {
   const [isLoading, setIsLoading] = useState(true);
 
+  const { mutateAsync } = useLikeImage();
+
   const handleImageLoad = useCallback(() => {
     setIsLoading(false);
   }, []);
+
+  const handleLike = useCallback(async () => {
+    try {
+      await mutateAsync({ imageId: node.id });
+    } catch (error) {
+      console.error(error);
+    }
+  }, [node.id, mutateAsync]);
 
   return (
     <figure key={node.title} className="image-container">
@@ -39,6 +51,7 @@ export const ImageContainer = ({ node }: ImageContainerProps) => {
             <FontAwesomeIcon
               className="like-icon"
               icon={node.liked ? faHeart : faHeartOutline}
+              onClick={handleLike}
             />
             <span>{node.likesCount}</span>
           </div>
---
+++ src/queries/useLikeImage.ts
@@ -0,0 +1,70 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import request, { gql } from "graphql-request";
+import { z } from "zod";
+import { imageSchema } from "../schemas";
+import { USE_GET_IMAGES_KEY } from "./useGetImages";
+
+export interface LikeImageParams {
+  imageId: string;
+  clientMutationId?: string;
+}
+
+const likeImageResponse = z.object({
+  likeImage: z.object({
+    image: imageSchema,
+    clientMutationId: z.string().nullable(),
+  }),
+});
+
+export type LikeImageResponse = z.infer<typeof likeImageResponse>;
+
+const likeImage = async ({ imageId, clientMutationId }: LikeImageParams) => {
+  const data = await request<LikeImageResponse>(
+    "https://sandbox-api-test.samyroad.com/graphql",
+    gql`
+      mutation LikeImage($imageId: ID!, $clientMutationId: String) {
+        likeImage(input: { imageId: $imageId, clientMutationId: $clientMutationId }) {
+          image {
+            author
+            id
+            liked
+            likesCount
+            picture
+            price
+            title
+          }
+          clientMutationId
+        }
+      }
+    `,
+    {
+      imageId,
+      clientMutationId,
+    },
+  );
+
+  const { data: parsedData, error } = likeImageResponse.safeParse(data);
+
+  if (error) {
+    console.error(error.errors);
+    throw new Error(error.errors.map((e) => e.message).join(", "));
+  }
+
+  return parsedData.likeImage;
+};
+
+export const USE_TOGGLE_LIKE_IMAGE_KEY = "likeImage";
+
+const useLikeImage = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationKey: [USE_TOGGLE_LIKE_IMAGE_KEY],
+    mutationFn: likeImage,
+    onSettled: async () => {
+      await queryClient.invalidateQueries({ queryKey: [USE_GET_IMAGES_KEY] });
+    },
+  });
+};
+
+export default useLikeImage;
